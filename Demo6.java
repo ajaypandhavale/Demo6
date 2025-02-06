d622c977e65225d0720ff6cff13651d27c6e8fc9
ajaypandhavale <aj40135821@gmail.com>
2025-02-06 13:28:32 +0530

    resetting to base code

--- Demo6.java
+++ Demo6.java
@@ -1,18 +1,5 @@
 public class Demo6 {
 public static void main(String[] args){
     System.out.println("Hii, Demo6");
-
-    System.out.println("Adding commit");
-
-
-    System.out.println("Adding commit4");
-
-    System.out.println("Adding commit5");
-
-    System.out.println("Adding commit6");
-
-    System.out.println("Adding commit8");
 }
-
-
 }
